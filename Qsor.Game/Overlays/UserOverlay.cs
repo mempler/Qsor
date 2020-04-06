8dfe8e393efea73ead6c33f57b11fe369f6a50a2
cyanidee <cyanide.space@gmail.com>
2020-04-06 09:38:04 +0200

    Clean up the UserOverlay

--- Qsor.Game/Overlays/UserOverlay.cs
+++ Qsor.Game/Overlays/UserOverlay.cs
@@ -20,7 +20,6 @@ namespace Qsor.Game.Overlays
         private DrawableAvatar _avatar;
         private DrawableLevelBar _levelBar;
         private User _activeUser;
-
         private Box _background;
         
         [BackgroundDependencyLoader]
@@ -31,50 +30,44 @@ namespace Qsor.Game.Overlays
             Width = 256;
             Height = 80;
             
-            _textFlowContainer = new TextFlowContainer { AutoSizeAxes = Axes.Both };
-            _textFlowContainer.Padding = new MarginPadding(4);
-            _textFlowContainer.AddText($"{_activeUser.Username}\n", text =>
+            _textFlowContainer = new TextFlowContainer
             {
-                text.Font = new FontUsage(size: 24, weight: "bold");
-            });
-            _textFlowContainer.AddText($"Performance: {_activeUser.Statistics.PerformancePoints}pp\n", text =>
-            {
-                text.Font = new FontUsage(size: 14);
-            });
-            _textFlowContainer.AddText($"Accuracy: {_activeUser.Statistics.Accuracy}%\n", text =>
-            {
-                text.Font = new FontUsage(size: 14);
-            });
-            _textFlowContainer.AddText($"Level: {_activeUser.Statistics.GetLevel()}", text =>
-            {
-                text.Font = new FontUsage(size: 14);
-            });
-            
-            _textFlowContainer.OriginPosition = new Vector2(-75,0);
+                AutoSizeAxes = Axes.Both,
+                Padding = new MarginPadding(4),
+                OriginPosition = new Vector2(-75,0)
+            };
 
-            AddInternal(_background = new Box
+            _textFlowContainer.AddParagraph($"{_activeUser.Username}", text => text.Font = new FontUsage(size: 24, weight: "Bold"));
+            _textFlowContainer.AddParagraph($"Performance: {_activeUser.Statistics.PerformancePoints}pp", text => text.Font = new FontUsage(size: 14));
+            _textFlowContainer.AddParagraph($"Accuracy: {_activeUser.Statistics.Accuracy:0.00}%", text => text.Font = new FontUsage(size: 14));
+            _textFlowContainer.AddParagraph($"Level: {_activeUser.Statistics.GetLevel()}", text => text.Font = new FontUsage(size: 14));
+
+            AddRangeInternal(new Drawable[]
             {
-                RelativeSizeAxes = Axes.Both,
-                Colour = Color4.White,
-                Alpha = 0f
-            });
-            AddInternal(_avatar = new DrawableAvatar());
-            AddInternal(new SpriteText
-            {
-                Font = new FontUsage(size: 32, weight: "Bold"),
-                Colour = Color4.DarkGray,
-                Text = new LocalisedString("#1"),
+                _background = new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = Color4.White,
+                    Alpha = 0f
+                },
+                _avatar = new DrawableAvatar(),
+                new SpriteText
+                {
+                    Font = new FontUsage(size: 32, weight: "Bold"),
+                    Colour = Color4.DarkGray,
+                    Text = new LocalisedString("#1"),
                 
-                Anchor = Anchor.BottomRight,
-                Origin = Anchor.BottomRight,
-                Padding = new MarginPadding(0),
-                Position = new Vector2(0, -5)
-            });
-            AddInternal(_textFlowContainer);
-            AddInternal(_levelBar = new DrawableLevelBar(_activeUser)
-            {
-                Anchor = Anchor.BottomRight,
-                Origin = Anchor.BottomRight,
+                    Anchor = Anchor.BottomRight,
+                    Origin = Anchor.BottomRight,
+                    Padding = new MarginPadding(0),
+                    Position = new Vector2(0, -5)
+                },
+                _textFlowContainer,
+                _levelBar = new DrawableLevelBar(_activeUser)
+                {
+                    Anchor = Anchor.BottomRight,
+                    Origin = Anchor.BottomRight,
+                }
             });
 
             _avatar.User.Value = _activeUser;
