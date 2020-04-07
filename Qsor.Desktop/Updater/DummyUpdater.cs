1b4e0fd6a2ca9486c1f15e7589b6d2f9ac237c4d
czapek <32851089+czapek1337@users.noreply.github.com>
2020-04-07 10:32:46 +0200

    Add a dummy updater (#27)

---
+++ Qsor.Desktop/Updater/DummyUpdater.cs
@@ -0,0 +1,19 @@
+﻿using osu.Framework.Allocation;
+using Qsor.Game.Updater;
+
+namespace Qsor.Desktop.Updater
+{
+   [Cached]
+   public class DummyUpdater : Game.Updater.Updater
+   {
+      public override void CheckAvailable()
+      {
+         BindableStatus.Value = UpdaterStatus.Latest;
+      }
+
+      public override void UpdateGame()
+      {
+         // It's a dummy updater! It doesn't do anything!
+      }
+   }
+}
