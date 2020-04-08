f8bebfeda7af29a304986eb01713d8fbc3872976
Robin A. P <me@mempler.de>
2020-04-08 06:43:29 +0200

    Implement Sentry error logging (#31)
    * Implement Sentry error logging
    
    * Removed exception for debugging

--- Qsor.Game/QsorBaseGame.cs
+++ Qsor.Game/QsorBaseGame.cs
@@ -1,4 +1,6 @@
-﻿using osu.Framework.Allocation;
+﻿using System;
+using System.Reflection;
+using osu.Framework.Allocation;
 using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
 using Qsor.Game.Beatmaps;
@@ -7,6 +9,7 @@ using Qsor.Game.Database;
 using Qsor.Game.Online;
 using Qsor.Game.Overlays;
 using Qsor.Game.Updater;
+using Qsor.Game.Utils;
 
 namespace Qsor.Game
 {
@@ -23,10 +26,13 @@ namespace Qsor.Game
         
         protected NotificationOverlay NotificationOverlay;
         protected UpdaterOverlay UpdaterOverlay;
+        protected SentryLogger SentryLogger;
 
         public Updater.Updater Updater;
         
         private DependencyContainer _dependencies;
+
+        public string Version => Assembly.GetEntryAssembly()?.GetName().Version?.ToString(3) ?? string.Empty;
 
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
             _dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
@@ -48,7 +54,8 @@ namespace Qsor.Game
             _dependencies.Cache(ConfigManager = new QsorConfigManager(storage));
             
             _dependencies.Cache(NotificationOverlay = new NotificationOverlay());
-
+            _dependencies.Cache(SentryLogger = new SentryLogger(this));
+            
             _dependencies.CacheAs(this);
             _dependencies.CacheAs(Host);
             
@@ -78,6 +85,8 @@ namespace Qsor.Game
                 ConfigManager?.Dispose();
                 BeatmapManager?.Dispose();
                 NotificationOverlay?.Dispose();
+                
+                SentryLogger?.Dispose();
             }
 
             base.Dispose(isDisposing);
