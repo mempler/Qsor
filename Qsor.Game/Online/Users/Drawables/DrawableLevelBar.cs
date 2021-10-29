6ed22179faf898faf689de2cb13eb299fcb43b1a
Mempler <me@mempler.de>
2021-10-29 06:37:51 +0200

    DrawableProgressbar

--- Qsor.Game/Online/Users/Drawables/DrawableLevelBar.cs
+++ Qsor.Game/Online/Users/Drawables/DrawableLevelBar.cs
@@ -1,16 +1,11 @@
 ﻿using osu.Framework.Allocation;
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osuTK.Graphics;
-using Qsor.Game.Online.Users;
+using Qsor.Game.Overlays.Drawables;
 
-namespace Qsor.Game.Overlays.Drawables
+namespace Qsor.Game.Online.Users.Drawables
 {
-    public class DrawableLevelBar : Container
+    public class DrawableLevelBar : DrawableProgressbar
     {
-        private Box _progressBox;
-        
         private readonly User _user;
 
         public DrawableLevelBar(User user)
@@ -21,36 +16,12 @@ namespace Qsor.Game.Overlays.Drawables
         [BackgroundDependencyLoader]
         private void Load()
         {
-            RelativeSizeAxes = Axes.X;
-            Height = 6;
-
-            Masking = true;
-
-            CornerRadius = 3;
-            CornerExponent = 2f;
-            
-            AddInternal(new Box
-            {
-                Colour = new Color4(1f, 1f, 1f, 0.5f),
-                RelativeSizeAxes = Axes.Both
-            });
-            
-            AddInternal(_progressBox = new Box
-            {
-                Colour = Color4.Yellow,
-                RelativeSizeAxes = Axes.Y,
-            });
+            Colour = Color4.Yellow;
         }
 
         protected override void LoadComplete()
         {
-            UpdateProgress();
-            base.LoadComplete();
-        }
-
-        public void UpdateProgress()
-        {
-            _progressBox.Width = (int) (DrawSize.X * _user.Statistics.GetProgress());
+            Current.Value = _user.Statistics.GetProgress();
         }
     }
 }
